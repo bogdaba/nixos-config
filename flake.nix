ae8c4cfb9c8a0a77679bb392459ca7576e43b1be
bogdaba <bogdaba@github.com>
2023-12-09 17:50:31 +0100

    first commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "How it sounds when you lose your sanity (in NixOS: The Dotfiles Descent) https://youtu.be/hFcLyDb6niA";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, home-manager, ... }@inputs: {
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+
+    nixosConfigurations = {
+
+      desktop = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./desktop/configuration.nix
+          ];
+      };
+    };
+  };
+}
