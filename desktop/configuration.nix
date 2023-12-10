dace694b7cb8c1417c600515f797b0b9323abb15
bogdaba <bogdaba@github.com>
2023-12-10 14:50:07 +0100

    poi

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -27,13 +27,14 @@
     };
   };
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.useOSProber = true;
+  #boot.loader.grub.enable = true;
+  #boot.loader.grub.device = "nodev";
+  #boot.loader.grub.useOSProber = true;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
