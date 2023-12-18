a2a646619c89eaefbf7e6f02c11de0eab25ba8df
bogdaba <bogdaba@github.com>
2023-12-18 21:42:46 +0100

    poi

--- home.nix
+++ home.nix
@@ -47,8 +47,15 @@
   };
 
   xdg.userDirs = {
-    enable = true;
-    
+  enable = true;
+  desktop      = "${config.home.homeDirectory}/desktop";
+  documents    = "${config.home.homeDirectory}/documents";
+  download     = "${config.home.homeDirectory}/download";
+  music        = "${config.home.homeDirectory}/music";
+  pictures     = "${config.home.homeDirectory}/pictures";
+  publicShare  = "${config.home.homeDirectory}/public";
+  templates    = "${config.home.homeDirectory}/templates";
+  videos       = "${config.home.homeDirectory}/videos";
   };
 
   # Add stuff for your user as you see fit:
