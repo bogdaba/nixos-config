e516eff9de2428613c5aa9562a4d441dc7c71601
bogdaba@github.com <bogdaba@github.com>
2024-02-02 20:41:50 +0100

    removed accidental <<<<<<HEAD etc

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -142,47 +142,6 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-<<<<<<< HEAD
-  obsidian
-  emacs
-  vim 
-  wget
-  calibre
-  hledger
-  qimgv
-  anki # the other one is outdated
-  libsForQt5.okular
-  doublecmd
-  syncplay
-  mc
-  git
-  keepassxc
-  firefox
-  #obsidian
-  gnome.gnome-tweaks
-  gnomeExtensions.vertical-workspaces # not sure if this does anything
-  p7zip
-  unzip
-  qbittorrent
-  celluloid
-  mpv
-  neofetch
-  mullvad-browser
-  libreoffice
-  copyq
-  yt-dlp
-  speedcrunch
-  qpdf
-  onlyoffice-bin
-  emacs    # Emacs 27.2
-  ripgrep
-  # optional dependencies
-  coreutils # basic GNU utilities
-  fd
-  clang
-  findutils
-  tts
-=======
     obsidian
 
     emacs
@@ -235,7 +194,6 @@ in
     qpdf
     onlyoffice-bin
     vscode-fhs
->>>>>>> 924ad2e (laptop poi)
   ];
 
   programs.firefox = {
