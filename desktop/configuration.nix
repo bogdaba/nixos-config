17825ed051a3b0f5fe3375a6ed85b8ccfc8cb1d1
bogdaba@github.com <bogdaba@github.com>
2024-02-02 23:02:49 +0100

    added environment variables

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -134,6 +134,17 @@ in
     "electron-25.9.0"
   ];
 
+  environment.homeBinInPath = true;
+  environment.sessionVariables = {
+    # MOZ_ENABLE_WAYLAND = "1"; # firefox
+    # Obsidian has pane bug with electron on. Also need to disable xwayland
+    # https://forum.obsidian.md/t/cannot-move-rearrange-panes-when-running-under-wayland/42377/55
+    # NIXOS_OZONE_WL = "1"; # electron - enabling
+    LEDGER_FILE = "/home/bork/vault/areas/finances/2024.journal";
+    # PATH = "/home/bork/scripts";
+    # QT_QPA_PLATFORM = "wayland";
+  };
+
   environment.systemPackages = with pkgs; [
     vim
     wget
