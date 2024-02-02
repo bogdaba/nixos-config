5c63e1b2f45bf7f0673e7754ddba03fc1cbe3fa3
bogdaba@github.com <bogdaba@github.com>
2024-02-02 17:05:36 +0100

    poi

--- flake.nix
+++ flake.nix
@@ -2,15 +2,13 @@
   description = "How it sounds when you lose your sanity (in NixOS: The Dotfiles Descent) https://youtu.be/hFcLyDb6niA";
 
   inputs = {
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # home-manager.url = "github:nix-community/home-manager";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-21.11";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, home-manager, ... }@inputs: {
-    # NixOS configuration entrypoint
+  outputs = { nixpkgs, nixpkgs-stable, home-manager, ... }@inputs: {
     # Available through 'nixos-rebuild --flake .#your-hostname'
 
     nixosConfigurations = {
