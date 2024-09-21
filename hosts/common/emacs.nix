c19e6f7b3271b9a00049c755220039a032f6948b
bogdaba <bogdaba@github.com>
2024-09-21 12:29:10 +0200

    Lightly change emacs package config

--- hosts/common/emacs.nix
+++ hosts/common/emacs.nix
@@ -1,24 +1,25 @@
 { inputs, config, lib, pkgs, ... }:
 
 {
-  services.emacs = {
-    enable = true;
-    # package = pkgs.emacs29;
-  };
-  environment.systemPackages = with pkgs; [
-    emacsPackages.vterm
-    cmake   # vterm so doctor doesn't complain
-    gnumake # vterm so doctor doesn't complain
-    emacsPackages.graphviz-dot-mode
-    graphviz # so doom doctor doesn't complain
-    emacsPackages.pipenv
-    pipenv
-    ripgrep
-    fd
-    coreutils
-    cmigemo # jap input
-    nixfmt-rfc-style # nix
-    ruby
-    shellcheck
-  ];
+        environment.systemPackages = with pkgs; [
+                emacsPackages.mozc
+                emacsPackages.vterm
+                cmake   # vterm so doctor doesn't complain
+                gnumake # vterm so doctor doesn't complain
+                emacsPackages.graphviz-dot-mode
+                graphviz # so doom doctor doesn't complain
+                emacsPackages.pipenv
+                pipenv
+                ripgrep
+                fd
+                coreutils
+                cmigemo # jap input
+                nixfmt-rfc-style # nix
+                ruby
+                shellcheck
+        ];
+        services.emacs = {
+                enable = true;
+                package = pkgs.emacs29-pgtk;
+        };
 }
