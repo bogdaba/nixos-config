8e43322c024ace821e0b550f1dccf096774b048b
bogdaba <bogdaba@github.com>
2024-09-21 12:28:34 +0200

    Fix wezterm config

--- home/config/wezterm/wezterm.lua
+++ home/config/wezterm/wezterm.lua
@@ -1,66 +1,68 @@
 
 local wezterm = require 'wezterm'
 return {
-    color_scheme = 'Dracula+',
-    -- color_scheme = 'Doom One',
-    enable_tab_bar = false,
-    -- font_size = 14.0,
-    font = wezterm.font('Fira Code'),
-    cursor_blink_rate = 0,
-    default_cursor_style = 'SteadyBlock',
-    -- macos_window_background_blur = 40,
-    -- macos_window_background_blur = 30,
+  enable_wayland = false,
+  color_scheme = 'Dracula+',
+  -- color_scheme = 'Doom One',
+  enable_tab_bar = false,
+  -- font_size = 14.0,
+  font = wezterm.font('Fira Code'),
+  cursor_blink_rate = 0,
+  default_cursor_style = 'SteadyBlock',
+  -- macos_window_background_blur = 40,
+  -- macos_window_background_blur = 30,
 
-    -- window_background_image = '/Users/omerhamerman/Downloads/3840x1080-Wallpaper-041.jpg',
-    -- window_background_image_hsb = {
-    -- 	brightness = 0.01,
-    -- 	hue = 1.0,
-    -- 	saturation = 0.5,
-    -- },
-    -- window_background_opacity = 0.92,
-    window_background_opacity = 1.0,
-    -- window_background_opacity = 0.78,
-    -- window_background_opacity = 0.20,
-    window_decorations = 'RESIZE',
-    keys = {
-        {
-            key = 'f',
-            mods = 'CTRL',
-            action = wezterm.action.ToggleFullScreen,
-        },
-        {
-            key = 'h',
-            mods = 'CTRL|SHIFT',
-            action = wezterm.action.ActivatePaneDirection 'Left',
-        },
-        {
-            key = 'l',
-            mods = 'CTRL|SHIFT',
-            action = wezterm.action.ActivatePaneDirection 'Right',
-        },
-        {
-            key = 'k',
-            mods = 'CTRL|SHIFT',
-            action = wezterm.action.ActivatePaneDirection 'Up',
-        },
-        {
-            key = 'j',
-            mods = 'CTRL|SHIFT',
-            action = wezterm.action.ActivatePaneDirection 'Down',
-        },
-        {
-        key = 'w',
-        mods = 'CTRL|SHIFT',
-        action = wezterm.action.CloseCurrentPane { confirm = true },
-        },
+  -- window_background_image = '/Users/omerhamerman/Downloads/3840x1080-Wallpaper-041.jpg',
+  -- window_background_image_hsb = {
+  -- 	brightness = 0.01,
+  -- 	hue = 1.0,
+  -- 	saturation = 0.5,
+  -- },
+  -- window_background_opacity = 0.92,
+  window_background_opacity = 1.0,
+  -- window_background_opacity = 0.78,
+  -- window_background_opacity = 0.20,
+  window_decorations = 'RESIZE',
+  -- ime_preedit_rendering = 'System', -- this needs to be builtin to work, which is the default value
+  keys = {
+    {
+      key = 'f',
+      mods = 'CTRL',
+      action = wezterm.action.ToggleFullScreen,
+    },
+    {
+      key = 'h',
+      mods = 'CTRL|SHIFT',
+      action = wezterm.action.ActivatePaneDirection 'Left',
+    },
+    {
+      key = 'l',
+      mods = 'CTRL|SHIFT',
+      action = wezterm.action.ActivatePaneDirection 'Right',
+    },
+    {
+      key = 'k',
+      mods = 'CTRL|SHIFT',
+      action = wezterm.action.ActivatePaneDirection 'Up',
+    },
+    {
+      key = 'j',
+      mods = 'CTRL|SHIFT',
+      action = wezterm.action.ActivatePaneDirection 'Down',
+    },
+    {
+      key = 'w',
+      mods = 'CTRL|SHIFT',
+      action = wezterm.action.CloseCurrentPane { confirm = true },
+    },
 
+  },
+  mouse_bindings = {
+    -- Ctrl-click will open the link under the mouse cursor
+    {
+      event = { Up = { streak = 1, button = 'Left' } },
+      mods = 'CTRL',
+      action = wezterm.action.OpenLinkAtMouseCursor,
     },
-    mouse_bindings = {
-        -- Ctrl-click will open the link under the mouse cursor
-        {
-        event = { Up = { streak = 1, button = 'Left' } },
-        mods = 'CTRL',
-        action = wezterm.action.OpenLinkAtMouseCursor,
-        },
-    },
+  },
 }
