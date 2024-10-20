95f5607bbb0826b0910b832cfa7ed7dd1098ae8e
bogdaba <bogdaba@github.com>
2024-10-20 10:52:37 +0200

    Add wsl profile

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -10,6 +12,7 @@
     self,
     nixpkgs,
     nixpkgs-unstable,
+    nixos-wsl,
     home-manager,
     ...
   } @ inputs: let
@@ -48,6 +51,12 @@
           specialArgs = { inherit inputs outputs;};
           modules = [
             ./hosts/lenovo/configuration.nix
+          ];
+        };
+        wsl = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs;};
+          modules = [
+            ./hosts/wsl/configuration.nix
           ];
         };
       };
